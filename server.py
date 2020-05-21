95489069985630635f273c3ffcdebe86fe57a181
djeck1432 <oleh.kostashchuk@gmail.com>
2020-05-21 18:29:39 +0200

    add new args argument log_name

--- server.py
+++ server.py
@@ -13,32 +13,39 @@ def get_environment_params():
     load_dotenv()
     parser = argparse.ArgumentParser(description='Setting log output')
     parser.add_argument(
-        '--logs',
-        action='store_true',
+        '--verbose',
+        '-v',
+        action='count',
         help='Switch on logging'
     )
     parser.add_argument(
         '--delay',
-        help='Install response delay for server'
+        help='Install response delay for server',
+        type=int
     )
     parser.add_argument(
-        'folders_path',
-        help='Write path to archives'
+        '--log_name',
+        help='Name of log file',
+        default='logs'
+    )
+    parser.add_argument(
+        '--folders_path',
+        help='Write path to archives',
+        type=str,
     )
     args = parser.parse_args()
-    if args.logs:
-        logging.basicConfig(
-            format=u'%(levelname)-8s %(message)s', level=logging.INFO, filename=u'logs.log'
-        )
-    return args.folders_path,int(args.delay)
+    logging.basicConfig(
+        format=u'%(levelname)-8s %(message)s', level=logging.INFO, filename=f'{args.log_name}.log'
+    )
+    return args.folders_path,args.delay
 
 async def archivate(request):
     response = web.StreamResponse()
     archive_name = request.match_info.get('archive_hash')
     folders_path,response_delay = get_environment_params()
+    # archive_path = os.path.join()
     if not os.path.exists(f'{folders_path}/{archive_name}'):
         raise HTTPNotFound(text='Ваша папка не найдена')
-
     response.headers['Content-Disposition'] = f'form-data; filename={archive_name}.zip'
     await response.prepare(request)
     cmd = f'zip -r  - {archive_name}'
@@ -53,11 +60,11 @@ async def archivate(request):
         while True:
             stdout_chunk = await process.stdout.read(chunk_b_size)
             await asyncio.sleep(response_delay)
-            if stdout_chunk:
-                await response.write(stdout_chunk)
-            else:
+            if not stdout_chunk:
                 logger.info('Archive downloaded success')
                 break
+            await response.write(stdout_chunk)
+
     except asyncio.CancelledError:
         logger.warning('Download was interrupted')
         process.kill()
