603ee14a442972875a5adc9f6d0d3a37399a45eb
djeck1432 <oleh.kostashchuk@gmail.com>
2020-05-21 19:18:49 +0200

    fix last bags

--- server.py
+++ server.py
@@ -9,8 +9,51 @@ from aiohttp.web_exceptions import HTTPNotFound
 
 logger = logging.getLogger('archive')
 
-def get_environment_params():
-    load_dotenv()
+
+async def archivate(request):
+    response = web.StreamResponse()
+    archive_name = request.match_info.get('archive_hash')
+    archive_path = os.path.join(app.folders_path,archive_name)
+    if not os.path.exists(archive_path):
+        raise HTTPNotFound(text='Ваша папка не найдена')
+    response.headers['Content-Disposition'] = f'form-data; filename={archive_name}.zip'
+    await response.prepare(request)
+    cmd = f'zip -r  - {archive_name}'
+    cwd = app.folders_path,
+    process = await asyncio.create_subprocess_exec(
+            *cmd.split(' '),
+            cwd=cwd,
+            stdout=asyncio.subprocess.PIPE,
+            stderr=asyncio.subprocess.PIPE,
+        )
+
+    chunk_b_size = 100 * 1024
+    try:
+        while True:
+            stdout_chunk = await process.stdout.read(chunk_b_size)
+            await asyncio.sleep(app.response_delay)
+            if not stdout_chunk:
+                logger.info('Archive downloaded success')
+                break
+            await response.write(stdout_chunk)
+
+    except asyncio.CancelledError:
+        logger.warning('Download was interrupted')
+        process.kill()
+        await process.communicate()
+        raise
+    finally:
+        return response
+
+
+async def handle_index_page(request):
+    async with aiofiles.open('index.html', mode='r') as index_file:
+        index_contents = await index_file.read()
+
+    return web.Response(text=index_contents, content_type='text/html')
+
+
+if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Setting log output')
     parser.add_argument(
         '--verbose',
@@ -34,55 +77,14 @@ def get_environment_params():
         type=str,
     )
     args = parser.parse_args()
+
     logging.basicConfig(
-        format=u'%(levelname)-8s %(message)s', level=logging.INFO, filename=f'{args.log_name}.log'
+        format=u'%(levelname)-8s %(message)s', level=args.verbose, filename=f'{args.log_name}.log'
     )
-    return args.folders_path,args.delay
-
-async def archivate(request):
-    response = web.StreamResponse()
-    archive_name = request.match_info.get('archive_hash')
-    folders_path,response_delay = get_environment_params()
-    # archive_path = os.path.join()
-    if not os.path.exists(f'{folders_path}/{archive_name}'):
-        raise HTTPNotFound(text='Ваша папка не найдена')
-    response.headers['Content-Disposition'] = f'form-data; filename={archive_name}.zip'
-    await response.prepare(request)
-    cmd = f'zip -r  - {archive_name}'
-    process = await asyncio.create_subprocess_shell(
-            cmd,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-            cwd=folders_path,
-        )
-    chunk_b_size = 100 * 1024
-    try:
-        while True:
-            stdout_chunk = await process.stdout.read(chunk_b_size)
-            await asyncio.sleep(response_delay)
-            if not stdout_chunk:
-                logger.info('Archive downloaded success')
-                break
-            await response.write(stdout_chunk)
-
-    except asyncio.CancelledError:
-        logger.warning('Download was interrupted')
-        process.kill()
-        await process.communicate()
-    finally:
-        return response
-
-
-async def handle_index_page(request):
-    async with aiofiles.open('index.html', mode='r') as index_file:
-        index_contents = await index_file.read()
-
-    return web.Response(text=index_contents, content_type='text/html')
-
-
-if __name__ == '__main__':
 
     app = web.Application()
+    app.response_delay = args.delay
+    app.folders_path = args.folders_path
     app.add_routes([
         web.get('/', handle_index_page),
         web.get('/archive/{archive_hash}/', archivate),
